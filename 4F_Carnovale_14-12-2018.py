089576a2aae6f096a591e2b0f140836579cf3556
Think1t <42834992+Think1t@users.noreply.github.com>
2018-12-14 08:44:49 +0100

    Delete 4F_Carnovale_14-12-2018.py

--- 4F_Carnovale_14-12-2018.py
+++
@@ -1,6 +0,0 @@
-# Questo programma richiede l'inserimento di un numero e stampa l'opposto (segno invertito)
-
-print ("Questo programma richiede l' inserimento di un numero e stampa l' opposto ")
-x= input ('Inserisci il valore: ')  # Inserimento del valore
-inv = -1 *int (x)   # Calcolo dell'opposto
-print ('Il valore con segno invertito è: ' + str(inv))  # Stampa dell'opposto
